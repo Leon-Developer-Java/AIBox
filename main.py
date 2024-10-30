96b48560740f41073b7facd8e9125c1c37db7022
Leon-Developer-Java <58179020+Leon-Developer-Java@users.noreply.github.com>
2024-10-30 16:43:47 +0800

    1. 界面可移动 2. 界面样式加阴影 3. page1-3切换

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt5.QtCore import Qt, QPoint, QEvent
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from view.login import *
 from view.main_window import *
@@ -40,6 +42,29 @@ class LoginWindow(QMainWindow):
 
 class MainWindow(QMainWindow):
 
+    # 事件过滤器函数
+    def eventFilter(self, source, event):
+        if source == self.topWidget:  # 检查事件是否来自 Top 小部件
+            if event.type() == QEvent.MouseButtonPress:
+                if event.button() == Qt.LeftButton:
+                    self.isDragging = True
+                    self.dragStartPosition = event.globalPos() - self.frameGeometry().topLeft()
+                    event.accept()
+                    return True
+            elif event.type() == QEvent.MouseMove:
+                if self.isDragging:
+                    self.move(event.globalPos() - self.dragStartPosition)
+                    event.accept()
+                    return True
+            elif event.type() == QEvent.MouseButtonRelease:
+                if event.button() == Qt.LeftButton:
+                    self.isDragging = False
+                    event.accept()
+                    return True
+
+        # 对于不是 Top 小部件的事件，或者我们不关心的事件类型，调用基类的 eventFilter
+        return super().eventFilter(source, event)
+
     def __init__(self):
         super().__init__()
 
@@ -49,16 +74,34 @@ class MainWindow(QMainWindow):
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
-        # self.shadow = QtWidgets.QGraphicsDropShadowEffect(self)
-        # self.shadow.setOffset(5, 5)
-        # self.shadow.setBlurRadius(10)
-        # self.shadow.setColor(QtCore.Qt.black)
-        # self.ui.frame.setGraphicsEffect(self.shadow)
+
+        # 获取顶部窗口部件后，将顶部窗口设置为按住可移动
+        self.topWidget = self.ui.TopFrame # 获取顶部窗口部件
+        self.topWidget.installEventFilter(self) # 安装事件过滤器
+        self.isDragging = False
+        self.dragStartPosition = QPoint()
+
+        # 设置窗口阴影效果
+        self.shadow = QtWidgets.QGraphicsDropShadowEffect(self) # 创建阴影效果对象
+        self.shadow.setOffset(0, 5) # 设置阴影偏移量
+        self.shadow.setBlurRadius(10) # 设置阴影模糊度
+        self.shadow.setColor(QtCore.Qt.black) # 设置阴影颜色
+        self.ui.MainFrame.setGraphicsEffect(self.shadow) # 设置窗口阴影效果
+
+
+
+        # 添加toolbutton点击事件，并初始化第一个按钮处于点击状态
+        self.ui.SystemInfo.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        self.ui.SystemInfo.setChecked(True)
+        self.ui.AI.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.Key.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+
+
 
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = LoginWindow()
+    window = MainWindow()
     sys.exit(app.exec())
